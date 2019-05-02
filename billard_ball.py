3d1cb1dd097c99b1da973973413bee3b210764c6
valholla75@gmail.com <valholla75@gmail.com>
2019-05-01 21:19:11 -0500

    Update style formatting and rename GraphPaper.py to graph_paper.py

--- billard_ball.py
+++ billard_ball.py
@@ -7,46 +7,47 @@
     <valholla75_at_gmail_dot_com>
 
     This script requests the number of a billiard ball, size (px)
-    and Font for the number.  Then Generates a billiard ball 
+    and Font for the number.  Then Generates a billiard ball
     with basic lightting, shadows and reflections.
     '''
 
 from gimpfu import *
 
 def generate_ball(size, ball, font):
+    '''Generate the Ball'''
     # Double the size for the width
-    size_x=size*2
+    size_x = size*2
 
     # Initialize the Progress Bar
     if ball < 1:
-        initstr="Generating Cue Ball..."
+        initstr = "Generating Cue Ball..."
     else:
-        initstr="Generating Ball "+str(ball)+"..."
+        initstr = "Generating Ball "+str(ball)+"..."
     gimp.progress_init(initstr)
 
     # Save User's Settings
     pdb.gimp_context_push()
 
     # Create new image, set resolution to 72ppi
-    ballimg=gimp.Image(size_x, size, RGB)
-    pdb.gimp_image_set_resolution(ballimg,72.0,72.0)
+    ballimg = gimp.Image(size_x, size, RGB)
+    pdb.gimp_image_set_resolution(ballimg, 72.0, 72.0)
 
     # Disable undo
     pdb.gimp_image_undo_disable(ballimg)
 
     # Generate Solid or Stripe Layer
-    layer0=gimp.Layer(ballimg, "Billard Ball", size_x, size, 
-            RGBA_IMAGE, 100, NORMAL_MODE)
+    layer0 = gimp.Layer(ballimg, "Billard Ball", size_x, size,
+                        RGBA_IMAGE, 100, NORMAL_MODE)
 
     # List of colors for the balls
     color_list = [
-                    (255, 255, 255), (255, 255, 0), (0, 0, 255), 
-                    (255, 0, 0), (160, 40, 170), (243, 153, 10), 
-                    (40, 150, 30), (150, 50, 50), (0, 0, 0), 
-                    (255, 255, 0), (0, 0, 255), (255, 0, 0), 
-                    (160, 40, 170), (243, 153, 10), (40, 150, 30), 
-                    (150, 50, 50)
-                 ]
+        (255, 255, 255), (255, 255, 0), (0, 0, 255),
+        (255, 0, 0), (160, 40, 170), (243, 153, 10),
+        (40, 150, 30), (150, 50, 50), (0, 0, 0),
+        (255, 255, 0), (0, 0, 255), (255, 0, 0),
+        (160, 40, 170), (243, 153, 10), (40, 150, 30),
+        (150, 50, 50)
+    ]
 
     # Set the foreground color to the color of the ball in the color_list
     pdb.gimp_context_set_foreground(color_list[ball])
@@ -58,9 +59,9 @@ def generate_ball(size, ball, font):
     layer0.fill(WHITE_FILL)
 
     # Add the layer to the image
-    ballimg.add_layer(layer0,-1)
+    ballimg.add_layer(layer0, -1)
 
-    # Check to see if the ball selected is a solid or stripe, 
+    # Check to see if the ball selected is a solid or stripe,
     # and fill the Layer correctly.
     if ball < 9:
         # Solid Layer
@@ -68,26 +69,26 @@ def generate_ball(size, ball, font):
     else:
         # Using Rect Select, Stripe will be the center 1/3 of the layer
         pdb.gimp_rect_select(ballimg,
-                0,                              # upper left x coordinate
-                (round(size/3)),                # upper left y coordinate
-                size_x,                         # width
-                (round(size/3)),                # height
-                CHANNEL_OP_ADD,                 # Mode
-                False,                          # feather
-                0)                              # feather radius
+                             0,                  # upper left x coordinate
+                             (round(size/3)),    # upper left y coordinate
+                             size_x,             # width
+                             (round(size/3)),    # height
+                             CHANNEL_OP_ADD,     # Mode
+                             False,              # feather
+                             0)                  # feather radius
         # Fill the Selection
         pdb.gimp_edit_fill(layer0, FOREGROUND_FILL)
         # Unselect the Stripe
         pdb.gimp_selection_none(ballimg)
-    
+
     # If Cue Ball do not need Number Circle or Number
     if ball > 0:
         # Number Circle Layer
-        layer1=gimp.Layer(ballimg, "Circle Layer", size_x, size, 
-                RGBA_IMAGE, 100, NORMAL_MODE)
+        layer1 = gimp.Layer(ballimg, "Circle Layer", size_x, size,
+                            RGBA_IMAGE, 100, NORMAL_MODE)
 
         # Add the layer to the image
-        ballimg.add_layer(layer1,-1)
+        ballimg.add_layer(layer1, -1)
 
         # Add an alpha Channel
         pdb.gimp_layer_add_alpha(layer1)
@@ -99,14 +100,14 @@ def generate_ball(size, ball, font):
         # the size will be 1/5th the requested size of the ball
         # The exact position will be centered later
         pdb.gimp_ellipse_select(ballimg,
-                round(size_x/2),     # upper left x coordinate
-                round(size/2),       # upper left y coordinate
-                size/5,              # width of ellipse
-                size/5,              # height of ellipse
-                CHANNEL_OP_ADD,      # Mode
-                True,                # antialias
-                False,               # feather
-                0)                   # festher radius
+                                round(size_x/2),  # upper left x coordinate
+                                round(size/2),    # upper left y coordinate
+                                size/5,           # width of ellipse
+                                size/5,           # height of ellipse
+                                CHANNEL_OP_ADD,   # Mode
+                                True,             # antialias
+                                False,            # feather
+                                0)                # festher radius
 
         # Fill the elipse Selection with White
         pdb.gimp_edit_fill(layer1, WHITE_FILL)
@@ -114,7 +115,7 @@ def generate_ball(size, ball, font):
         # Crop and Center the layer
         pdb.plug_in_autocrop_layer(ballimg, layer1)
         layer1.set_offsets(int(size_x/2-layer1.width/2),
-                int(size/2-layer1.height/2))
+                           int(size/2-layer1.height/2))
 
         # Ball Number
         # Set Foreground to Black for Ball Number
@@ -122,57 +123,57 @@ def generate_ball(size, ball, font):
 
         # Create Number Text layer
         # Exact Position will be centered later
-        number_layer=pdb.gimp_text_fontname(ballimg, None,
-                round(size_x/2),        # upper left x corrdinate 
-                round(size/2),          # upper left y coordinate 
-                ball,                   # text
-                0,                      # border
-                True,                   # antialias
-                size/8,                 # text size
-                PIXELS,                 # size units
-                font)                   # font name
-    
+        number_layer = pdb.gimp_text_fontname(ballimg, None,
+                                              round(size_x/2),  # upper left x corrdinate
+                                              round(size/2),    # upper left y coordinate
+                                              ball,             # text
+                                              0,                # border
+                                              True,             # antialias
+                                              size/8,           # text size
+                                              PIXELS,           # size units
+                                              font)             # font name
+
         # Center the Number layer
         number_layer.set_offsets(int(size_x/2-number_layer.width/2),
-                int(size/2-number_layer.height/2))
-    
+                                 int(size/2-number_layer.height/2))
+
     # Merge all visible layers
-    layer3=pdb.gimp_image_merge_visible_layers(ballimg,EXPAND_AS_NECESSARY)
-    
+    layer3 = pdb.gimp_image_merge_visible_layers(ballimg, EXPAND_AS_NECESSARY)
+
     # Map to Sphere
-    pdb.plug_in_map_object(ballimg, layer3, 
-            1,                          # mapping 1=sphere
-            0.5, 0.5, 2.0,              # viewpoint x,y,z
-            0.5, 0.5, 0.9,              # object pos x,y,z
-            1.0, 0.0, 0.0,              # first axis x,y,z
-            0.0, 1.0, 0.0,              # second axis x,y,z
-            0.0, -10.0, -5.0,           # axis rotation x,y,z
-            0,                          # light type 0=point
-            (255, 255, 255),            # light color (r,g,b)
-            -0.5, -0.5, 2.0,            # light position x,y,z
-            1.0, 1.0, 1.0,              # light direction x,y,z
-            0.4,                        # ambient-intensity
-            0.6,                        # diffuse-intenstity
-            0.5,                        # diffuse-reflectivity
-            0.5,                        # specular-reflectifity
-            27.0,                       # highlight
-            True,                       # antialias
-            False,                      # tiled
-            False,                      # new image
-            True,                       # transparent background
-            0.250,                      # radius
-            1.0,                        # x-scale
-            1.0,                        # y-scale
-            1.0,                        # z-scale
-            0,                          # unused for sphere
-            layer3,                     # unused for sphere
-            layer3,                     # unused for sphere
-            layer3,                     # unused for sphere
-            layer3,                     # unused for sphere
-            layer3,                     # unused for sphere
-            layer3,                     # unused for sphere
-            layer3,                     # unused for sphere
-            layer3)                     # unused for sphere
+    pdb.plug_in_map_object(ballimg, layer3,
+                           1,                   # mapping 1=sphere
+                           0.5, 0.5, 2.0,       # viewpoint x,y,z
+                           0.5, 0.5, 0.9,       # object pos x,y,z
+                           1.0, 0.0, 0.0,       # first axis x,y,z
+                           0.0, 1.0, 0.0,       # second axis x,y,z
+                           0.0, -10.0, -5.0,    # axis rotation x,y,z
+                           0,                   # light type 0=point
+                           (255, 255, 255),     # light color (r,g,b)
+                           -0.5, -0.5, 2.0,     # light position x,y,z
+                           1.0, 1.0, 1.0,       # light direction x,y,z
+                           0.4,                 # ambient-intensity
+                           0.6,                 # diffuse-intenstity
+                           0.5,                 # diffuse-reflectivity
+                           0.5,                 # specular-reflectifity
+                           27.0,                # highlight
+                           True,                # antialias
+                           False,               # tiled
+                           False,               # new image
+                           True,                # transparent background
+                           0.250,               # radius
+                           1.0,                 # x-scale
+                           1.0,                 # y-scale
+                           1.0,                 # z-scale
+                           0,                   # unused for sphere
+                           layer3,              # unused for sphere
+                           layer3,              # unused for sphere
+                           layer3,              # unused for sphere
+                           layer3,              # unused for sphere
+                           layer3,              # unused for sphere
+                           layer3,              # unused for sphere
+                           layer3,              # unused for sphere
+                           layer3)              # unused for sphere
 
     # Scale the sphere to selected Size
     pdb.gimp_image_scale(ballimg, size, size)
@@ -198,8 +199,8 @@ register(
         "",                          # Image Types "" for new
         [
             (PF_INT, "size", "Size(px)", 256),
-            (PF_OPTION, "ball", "Ball Number", 0, 
-                ("cue",1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)),
+            (PF_OPTION, "ball", "Ball Number", 0,
+             ("cue", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)),
             (PF_FONT, "font", "Number Font", "Comic Sans MS")
         ],                           # User Inputs
         [],                          # Resuts
--- graph_paper.py
+++ graph_paper.py
@@ -8,7 +8,7 @@
 from gimpfu import *
 
 def generate_graph_paper(width, height, grid_size):
-    ''' Generate a new Image containing the Graph Paper 
+    ''' Generate a new Image containing the Graph Paper
         with the user supplied parameters, and display the new image
 
     Parameters:
@@ -23,17 +23,17 @@ def generate_graph_paper(width, height, grid_size):
     pdb.gimp_context_push()
 
     ## Create the Image, Set the Resolution to 72ppi
-    img=gimp.Image(width, height, RGB)
-    pdb.gimp_image_set_resolution(img,72.0,72.0)
+    img = gimp.Image(width, height, RGB)
+    pdb.gimp_image_set_resolution(img, 72.0, 72.0)
 
     ## We are creating a new image: Disable UNDO
     pdb.gimp_image_undo_disable(img)
 
     ## Create the Drawable Layer and Add to the Image
-    background=gimp.Layer(img, "Grid", width, height, 
-            RGB_IMAGE, 100, NORMAL_MODE)
+    background = gimp.Layer(img, "Grid", width, height,
+                            RGB_IMAGE, 100, NORMAL_MODE)
     background.fill(WHITE_FILL)
-    img.add_layer(background,-1)
+    img.add_layer(background, -1)
 
     ## Configure the Brush for Grid Lines, Brush Name, Size and Color
     pdb.gimp_context_set_brush("1. Pixel")
@@ -41,38 +41,38 @@ def generate_graph_paper(width, height, grid_size):
     pdb.gimp_context_set_foreground((0, 0, 0))
 
     ## Number of Horizontal and Vertical lines
-    vlines=round(width/grid_size)
-    hlines=round(height/grid_size)
+    vlines = round(width/grid_size)
+    hlines = round(height/grid_size)
 
-    ## Init Progress 
-    progress=1
+    ## Init Progress
+    progress = 1
 
     ## Add Virtical Lines
-    i=0
+    i = 0
     ## We want the Grids to be centered on the Canvas
-    vpoint=round((width-(grid_size*vlines))/2)
+    vpoint = round((width-(grid_size*vlines))/2)
 
     while i <= vlines:
         ## Do not draw line if along the edge of Canvas
-        if (vpoint>0) & (vpoint<width):
-            pdb.gimp_pencil(background, 4, [vpoint,0,vpoint,height])
+        if (vpoint > 0) & (vpoint < width):
+            pdb.gimp_pencil(background, 4, [vpoint, 0, vpoint, height])
             gimp.progress_update(progress/(vlines+hlines))
-        i+=1
-        progress+=1
-        vpoint+=grid_size
+        i += 1
+        progress += 1
+        vpoint += grid_size
 
     ## Add Horizontal Lines
-    i=0
+    i = 0
     ## We want the Grids to be centered on the Canvas
-    hpoint=round((height-(grid_size*hlines))/2)
+    hpoint = round((height-(grid_size*hlines))/2)
     while i <= hlines:
         ## Do not draw line if along the edge of Canvas
-        if (hpoint>0) & (hpoint<height):
-            pdb.gimp_pencil(background, 4, [0,hpoint,width,hpoint])
+        if (hpoint > 0) & (hpoint < height):
+            pdb.gimp_pencil(background, 4, [0, hpoint, width, hpoint])
             gimp.progress_update(progress/(vlines+hlines))
-        i+=1
-        progress+=1
-        hpoint+=grid_size
+        i += 1
+        progress += 1
+        hpoint += grid_size
 
     ## ReEnable UNDO
     pdb.gimp_image_undo_enable(img)
@@ -88,18 +88,17 @@ register(
         "python_fu_generate_graph_paper",
         "Graph Paper Generator",
         "Generate Graph Paper",
-        "Mike Watters", 
-        "Mike Watters", 
+        "Mike Watters",
+        "Mike Watters",
         "2018",
         "Graph Paper...",
-        " ", 
+        " ",
         [
             (PF_INT, "number", "Width (px)", 612), ## default 8.5"
             (PF_INT, "number", "Height (px)", 792), ## default 11"
             (PF_INT, "number", "Size of Grid (px)", 72) ## default 1" square
-        ], 
+        ],
         [],
         generate_graph_paper, menu="<Image>/File/Create")
 
 main()
-
